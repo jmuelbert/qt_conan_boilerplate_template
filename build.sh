677e0e5dce977cd46ac70dba4a1e6bf0016206a2
Jürgen <juergen.muelbert@gmail.com>
2022-06-26 10:23:32 +0200

    Improve build

--- build.sh
+++ build.sh
@@ -14,11 +14,15 @@ conan install ../conanfile.py -b missing -pr:b default -s build_type="${BUILD_TY
 cd ..
 cmake -S . -B ./build -G "${GENERATOR}" \
     -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} \
+    -DCMAKE_INSTALL_PREFIX:PATH="./usr" \
     -DENABLE_DEVELOPER_MODE:BOOL=${DEVELOPER_MODE} \
     -DOPT_ENABLE_COVERAGE:BOOL=${COVERAGE} \
     -DENABLE_TESTING:BOOL=${TESTING} \
     -DCMAKE_TOOLCHAIN_FILE:PATH="./build/generators/conan_toolchain.cmake"
-cmake --build ./build
+cmake --build ./build --config ${BUILD_TYPE} --target install
 cd build || exit
 ctest -VV -C "${BUILD_TYPE}"
+cpack -C ${BUILD_TYPE} -G TBZ2
+cpack -C ${BUILD_TYPE} -G DragNDrop
+# cpack -C ${BUILD_TYPE} -G IFW
 cd ..

986963480749a1693d4680e5107652e1f01f6dc2
Jürgen <juergen.muelbert@gmail.com>
2022-11-11 12:54:18 +0100

    set minimal conan version to 1.52

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import copy, load, rmdir
 from conan.tools.scm import Version
 
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=1.50.0"
 
 
 class QtTestConan(ConanFile):

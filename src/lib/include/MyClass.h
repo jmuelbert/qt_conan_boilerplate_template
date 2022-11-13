a6599636918a8625a00aba5d2428eb547fcf30b7
Jürgen Mülbert <jmuelbert@users.noreply.github.com>
2022-11-13 14:03:24 +0100

    Create MyClass.h

---
+++ src/lib/include/MyClass.h
@@ -0,0 +1,26 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Project qt_conan_boilerplate_template, Jürgen Mülbert
+ *
+ * SPDX-License-Identifier: EUPL-1.2
+ *
+ */
+
+#pragma once
+
+#include <QString>
+#include <string>
+
+namespace mApp
+{
+class MyClass
+{
+    QString _text;
+
+public:
+    explicit MyClass(QString text);
+
+    [[nodiscard]] QString appendIt(const QString &extra) const;
+    [[nodiscard]] QString text() const;
+};
+
+} // namespace mApp
